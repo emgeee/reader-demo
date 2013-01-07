e0638d1bfdebaca8f9e782f7b6e46240febb0ecf
Matt Green <mgreen9@gmail.com>
2013-01-07 13:20:56 -0500

    made document_id a non-protected attribute

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -1,5 +1,5 @@
 class Annotation < ActiveRecord::Base
-  attr_accessible :comment, :length, :name, :position
+  attr_accessible :comment, :length, :name, :position, :document_id
 
   belongs_to :documents
 end
