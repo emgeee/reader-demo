5b62074573f2748b3b7486d72385053abb260309
Matt Green <mgreen9@gmail.com>
2013-01-25 14:03:31 -0500

    added some more code, still broken

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,4 +3,11 @@ class Document < ActiveRecord::Base
   has_attached_file :epub
 
   has_many :annotations
+
+  before_post_process :set_content_type
+
+  def set_content_type
+    self.epub.instance_write(:content_type, MIME::Types.type_for(self.epub).to_s)
+  end
+  
 end
