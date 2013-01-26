ae5aca2a85e3301d11622d0535e790b16362e25f
Matt Green <mgreen9@gmail.com>
2013-01-26 10:13:27 -0500

    updated form and changed migration

--- app/models/document.rb
+++ app/models/document.rb
@@ -4,7 +4,7 @@ class Document < ActiveRecord::Base
 
   has_many :annotations
 
-  before_post_process :set_content_type
+#  before_post_process :set_content_type
 
   def set_content_type
     self.epub.instance_write(:content_type, MIME::Types.type_for(self.epub).to_s)
--- db/migrate/20130125182655_add_attachment_epub_to_documents.rb
+++ db/migrate/20130125182655_add_attachment_epub_to_documents.rb
@@ -1,11 +1,5 @@
 class AddAttachmentEpubToDocuments < ActiveRecord::Migration
-  def self.up
-    change_table :documents do |t|
-      t.has_attached_file :epub
-    end
-  end
-
-  def self.down
-    drop_attached_file :documents, :epub
+  def change
+    add_attachment :documents, :epub
   end
 end
